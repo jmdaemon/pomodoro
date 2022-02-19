8233f3cb4af391a373a93e87419ae9dc37ce20de
Joseph Diza <josephm.diza@gmail.com>
2022-02-19 15:09:52 -0800

    Pass Timer struct to threadfn

--- src/pomodoro.c
+++ src/pomodoro.c
@@ -5,46 +5,64 @@
 #include <unistd.h>
 
 typedef struct Timer {
-  int shortbreak;
-  int longbreak;
-  int interval_count;
-
-  /*int shortbreak = to_secs(10);*/
-  /*int longbreak = to_secs(30);*/
+  int *shortbreak;
+  int *longbreak;
+  int *interval_count;
 } Timer;
 
 int to_secs(int mins) { return mins * 60; }
 
 void *threadfn(void *vargp) {
-  Timer *timer = malloc(sizeof(vargp));
-  /*sleep(1);*/
-  /*(timer->interval_count == 4) ? sleep(timer->longbreak) :
-   * sleep(timer->shortbreak);*/
-  while (timer->interval_count <= 4) {
-    if (timer->interval_count == 4) {
-      sleep(timer->longbreak);
-      timer->interval_count = 0;
-      printf("Pomodoro #%d\n", timer->interval_count);
+  /*Timer *timer = malloc(sizeof(vargp));*/
+  Timer *timer = vargp;
+  /*printf("%d %d\n", *timer->shortbreak, *timer->longbreak);*/
+
+  while (*timer->interval_count <= 4) {
+    /*printf("%d %d\n", *timer->shortbreak, *timer->longbreak);*/
+    if (*timer->interval_count == 4) {
+      sleep(*(timer->longbreak));
+      (*(timer->interval_count)) = 0;
+      printf("Pomodoro #%d\n", *timer->interval_count);
       break;
     } else
-      sleep(timer->shortbreak);
+      sleep(*timer->shortbreak);
 
-    timer->interval_count++;
-    printf("Pomodoro #%d\n", timer->interval_count);
+    (*timer->interval_count)++;
+    printf("Pomodoro #%d\n", *timer->interval_count);
   }
   printf("Pomodoro Interval Complete\n");
   free(timer);
   return NULL;
 }
 
-int main(int argc, char **argv) {
-  /*Timer timer = { to_secs(10), to_secs(30) };*/
+int main() {
   Timer *timer = malloc(sizeof(Timer));
+  /*Timer tstruct = { };*/
+  /*Timer * timer = &tstruct;*/
+  /**(timer->shortbreak) = to_secs(10);*/
+  /**(timer->longbreak) = to_secs(30);*/
+  int shortbreak = to_secs(10);
+  int longbreak = to_secs(30);
+  int interval_count = 0;
+
+  timer->shortbreak = &shortbreak;
+  timer->longbreak = &longbreak;
+  timer->interval_count = &interval_count;
+
+  /*timer->shortbreak = &shortbreak;*/
+  /*timer->longbreak = &longbreak;*/
+  /**timer->shortbreak = shortbreak;*/
+  /**timer->longbreak = longbreak;*/
+
+  /*printf("%d %d\n", *timer->shortbreak, *timer->longbreak);*/
 
   pthread_t thread_id;
   printf("Before Thread\n");
   pthread_create(&thread_id, NULL, threadfn, timer);
+  /*if (pthread_create(&thread_id, NULL, threadfn, timer))*/
+    /*free(timer);*/
   pthread_join(thread_id, NULL);
   printf("After Thread\n");
+  /*free(timer);*/
   exit(0);
 }
