7ad758bbe0891df11df42b7ebf2243caca72063a
Joseph Diza <josephm.diza@gmail.com>
2022-03-27 20:47:38 -0700

    Include ProgressBar in Timer

--- include/pomodoro.h
+++ include/pomodoro.h
@@ -7,11 +7,13 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include "progressbar.h"
 
 typedef struct Timer {
   int *shortbreak;
   int *longbreak;
   int *interval_count;
+  ProgressBar *pb;
 } Timer;
 
 int to_secs(int mins);
